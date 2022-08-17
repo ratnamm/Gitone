ba7a4530fbafaad348326e9a57a75007162e6f2c
ratnamm <madhu.ratnam@gmail.com>
2022-08-17 19:24:06 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -8,10 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
     print("Hello madhu")
-<<<<<<<<< Temporary merge branch 1
-=========
-    print("New line added ")
->>>>>>>>> Temporary merge branch 2
+
+
 
 
 
